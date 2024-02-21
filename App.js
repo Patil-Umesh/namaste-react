a2594c8b90548574108ac76612338b6ddd40d6ec
Umesh-Patil2021 <umesh.patil@excellarate.com>
2024-02-21 12:46:52 +0530

    Igniting our app using Parcel

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
-/**
- *  <div id='parent'>
- *     <div id='child'>
- *        <h1>I'm an H1 tag</h1>
- *        <h2>I'm an H2 tag</h2>
- *     </div>
-    </div>
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child-1" }, [
@@ -13,8 +7,8 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {}, "Im an H2 tag"),
   ]),
   React.createElement("div", { id: "child-2" }, [
-    React.createElement("h1", {}, "Im an H1 tag"),
-    React.createElement("h2", {}, "Im an H2 tag"),
+    React.createElement("h1", {}, "Im an H1 tag from child 2"),
+    React.createElement("h2", {}, "Im an H2 tag from child 2"),
   ]),
 ]);
 
